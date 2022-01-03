e9c3d235bc8eba2aaca35b21e553529a80511b6e
nadavhaik <67607457+nadavhaik@users.noreply.github.com>
2022-01-03 18:09:42 +0200

    Update AbstractContent.java

--- src/main/java/bgu/spl/net/impl/objects/AbstractContent.java
+++ src/main/java/bgu/spl/net/impl/objects/AbstractContent.java
@@ -28,28 +28,9 @@ public abstract class AbstractContent extends ObjectWithCreationTime {
     }
 
     private String filter(String content) {
-        StringBuilder sb = new StringBuilder(content);
-        for(String word : offensiveWords) {
-            // Edge case -  Word equals whole content
-            if(sb.toString().equals(word)) {
-                sb = new StringBuilder(replacementWord);
-                continue;
-            }
-
-            // 1. Word is the prefix
-            if(sb.toString().startsWith(word + " "))
-                sb.replace(0, word.length()-1, replacementWord);
-            // 2. Word has occurrences inside
-            for(int start = sb.indexOf(" " + word + " "); start != -1;
-                    start = sb.indexOf(" " + word + " ")) {
-                int end = start + 1 + (word.length()-1) + 1; // including both spaces
-                sb.replace(start+1, end-1, replacementWord);
-            }
-            // 3. Word is the postfix
-            if(sb.toString().endsWith(" " + word))
-                sb.replace(sb.length()-word.length(), sb.length()-1, replacementWord);
-        }
-        return sb.toString();
+        for(String word : offensiveWords)
+            content = content.replace(word, replacementWord);
+        return content;
     }
 
     public String getContent() {
