6d091d34c07778f45be442bf9e761ac62e24cda0
nadavhaik <67607457+nadavhaik@users.noreply.github.com>
2022-01-06 17:33:04 +0200

    שינוי נתיב מיין

--- src/main/java/bgu/spl/net/impl/BGSServer/TCPMain.java
+++ src/main/java/bgu/spl/net/impl/BGSServer/TCPMain.java
@@ -1,4 +1,4 @@
-package bgu.spl.net.main;
+package bgu.spl.net.impl.BGSServer;
 
 import bgu.spl.net.api.impl.EncoderDecoderImpl;
 import bgu.spl.net.api.impl.MessagingProtocolImpl;
@@ -6,17 +6,16 @@ import bgu.spl.net.srv.Server;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
-public class ReactorMain {
+public class TCPMain {
     public static void main(String[] args) throws UnknownHostException {
-        if(args.length < 2) {
-            System.out.println("Usage: port, threads");
+        if(args.length == 0) {
+            System.out.println("Usage: port");
             return;
         }
         int port = Integer.parseInt(args[0]);
-        int numOfThreads = Integer.parseInt(args[1]);
         InetAddress localhost = InetAddress.getLocalHost();
         System.out.println("SYSTEM IP: " + localhost.getHostAddress());
-        Server.reactor(numOfThreads, port, MessagingProtocolImpl::new,
+        Server.threadPerClient(port, MessagingProtocolImpl::new,
                 EncoderDecoderImpl::new).serve();
     }
 }
